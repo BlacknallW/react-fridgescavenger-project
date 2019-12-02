28cf4aaf4ad47c229f9c0ffdc27349df3c97223e
William Blacknall <williamblacknall@gmail.com>
2019-12-02 12:20:30 -0500

    Added dynamic paging functionality, next step is populating pages

--- pages/index.js
+++ pages/index.js
@@ -2,33 +2,39 @@ import React from "react";
 import Head from "next/head";
 import Nav from "../components/nav";
 import fetch from "isomorphic-unfetch";
+import Link from "next/link";
 
-const Home = (props) => (
+const Home = props => (
 	<div>
 		<Head>
 			<title>Home</title>
 			<link rel="icon" href="/favicon.ico" />
 		</Head>
-    <Nav />
-    <h1>Recipe List</h1>
-    <ol>
-      {props.recipes.map(recipe => (
-        <li key={recipe.id}>{recipe.title}</li>
-      ))}
-    </ol>
-		
+		<Nav />
+		<h1>Recipe List</h1>
+		<ol>
+			{props.recipes.map(recipe => (
+				<li key={recipe.id}>
+					<Link href="/p/[id]" as={`/p/${recipe.id}`}>
+						<a>{recipe.title}</a>
+					</Link>
+				</li>
+			))}
+		</ol>
 	</div>
 );
 
 Home.getInitialProps = async () => {
-  const res = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=cbdb4c6c328541689e7db2d210a6b528&query=cheese&number=10&instructionsRequired=true`);
-  const data = await res.json();
+	const res = await fetch(
+		`https://api.spoonacular.com/recipes/complexSearch?apiKey=cbdb4c6c328541689e7db2d210a6b528&query=cheese&number=10&instructionsRequired=true`
+	);
+	const data = await res.json();
 
-  console.log(data.results)
+	console.log(data.results);
 
-  return {
-    recipes: data.results.map(result => result)
-  }
-}
+	return {
+		recipes: data.results.map(result => result)
+	};
+};
 
 export default Home;
