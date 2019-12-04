247f21e146b5ea31140d8b94674c0d43ceb4b7eb
William Blacknall <williamblacknall@gmail.com>
2019-12-04 08:54:22 -0500

    Added SearchBar, using React hooks to connect functionality

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Head from "next/head";
 import Nav from "../components/nav";
 import fetch from "isomorphic-unfetch";
 import Link from "next/link";
+import SearchBar from "../components/SearchBar"
+import Axios from "axios"
 // import { baseUrl } from "../config";
 
 
@@ -15,25 +17,34 @@ const Home = props => (
 		</Head>
 		<Nav />
 		<h1>Recipe List</h1>
+		<SearchBar onSubmit={OnSearchSubmit()}/>
 		<ol>
-			{props.recipes.map(recipe => (
+			{/* {props.recipes.map(recipe => (
 				<li key={recipe.id}>
 					<Link href="/p/[id]" as={`/p/${recipe.id}`}>
 						<a>{recipe.title}</a>
 					</Link>
 				</li>
-			))}
+			))} */}
 		</ol>
 	</div>
 );
 
-Home.getInitialProps = async () => {
-	const res = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=cbdb4c6c328541689e7db2d210a6b528&query=cheese&number=10&instructionsRequired=true`);
-	const data = await res.json();
+const OnSearchSubmit = async (query) => {
+	const res = await Axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=cbdb4c6c328541689e7db2d210a6b528&query=${query}&number=10&instructionsRequired=true`);
 
-	return {
-		recipes: data.results.map(result => result)
-	};
-};
+	console.log(res.data)
+
+	return res.data
+}
+
+// Home.getInitialProps = async (query) => {
+// 	const res = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=cbdb4c6c328541689e7db2d210a6b528&query=${query}&number=10&instructionsRequired=true`);
+// 	const data = await res.json();
+
+// 	return {
+// 		recipes: data.results.map(result => result)
+// 	};
+// };
 
 export default Home;
