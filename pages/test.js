f4176824bbc09f016e7e31099a127d47fad7b961
William Blacknall <williamblacknall@gmail.com>
2019-12-10 20:54:16 -0500

    Successfully called NewsAPI in test environment. Going to set it up as a React component and hopefully have news articles appear unobtrusively along the bottom of the page.

--- pages/test.js
+++ pages/test.js
@@ -1,11 +1,29 @@
 import React, {useState, useEffect} from "react";
+import Axios from "axios"
+
 import "../styles/styles.sass";
 
-
-export default function Test() {
+export default function Test(props) {
 	return (
 		<>
+        <ol>
+            {props.articles.map(article => 
+                <li>
+                    {article.description}
+                </li>)}
+        </ol>
+        
 		</>
 	);
 }
 
+Test.getInitialProps = async () => {
+    const res = await Axios.get(`https://newsapi.org/v2/everything?q=cooking&sortBy=relevancy&apiKey=2412b7d48fe045f08fc92bc9140f561a`)
+
+    const articles = res.data.articles
+
+    console.log(typeof articles)
+    return {articles}
+
+    
+}
