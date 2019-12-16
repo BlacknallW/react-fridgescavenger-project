130240b2089cc386f024c83bf0aaf3b49ab51bfd
William Blacknall <williamblacknall@gmail.com>
2019-12-16 11:55:57 -0500

    Removed all references to express, as that will no longer be used in this project now that firebase is actually working. This is now a 100% NextJS file system. Also finished the barebones functionality of adding users.

--- components/NavBar.js
+++ components/NavBar.js
@@ -5,16 +5,17 @@ export default function Navbar() {
 		<nav className="navbar" role="navigation" aria-label="main navigation">
 			<div className="navbar-brand">
 				<a className="navbar-item" href="/">
-                    <figure>
-					<img src="https://i.imgur.com/reKZlmN.png" /></figure>
+					<figure>
+						<img src="https://i.imgur.com/reKZlmN.png" />
+					</figure>
 				</a>
 
-					<a href="/" className="navbar-item">
-						Recipes
-					</a>
-					<a href="/scavenge" className="navbar-item">
-						Scavenge
-					</a>
+				<a href="/" className="navbar-item">
+					Recipes
+				</a>
+				<a href="/scavenge" className="navbar-item">
+					Scavenge
+				</a>
 			</div>
 		</nav>
 	);
